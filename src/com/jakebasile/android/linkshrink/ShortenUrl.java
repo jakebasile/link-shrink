4a6ba4a516cc691721ed0b57223d9cf1f89a76c3
Jake Basile <jakebasile@me.com>
2011-04-27 18:04:27 -0400

    Renamed to Link Shrink Pro, package renamed to com.jakebasile.android.linkshrink.
    I'm pretty sad this needed to be done, but there's been no response from Google.

--- src/com/jakebasile/android/linkshrink/ShortenUrl.java
+++ src/com/jakebasile/android/linkshrink/ShortenUrl.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jakebasile.android.urlshortener;
+package com.jakebasile.android.linkshrink;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
